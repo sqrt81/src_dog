a4678b8be23768a3ba96431e2e71c68acc90c142
sqrt81 <hanxy17@mails.tsinghua.edu.cn>
2020-10-16 14:57:19 +0800

    docs(ClassDeclare): add a rule of the classes's behaviors

--- dog_control/include/dog_control/utils/ClassDeclare.h
+++ dog_control/include/dog_control/utils/ClassDeclare.h
@@ -14,6 +14,17 @@
         In the header file of a module, it is forbidden to
         include module header files or to make forward declarations
         of other modules. If needed, include this file instead.
+
+    In modules, it is common to leave interfaces for inputing or
+    outputing data. However, in order to keep the logic clear,
+    we here suggest every module to use only "query" interfaces.
+    That is to say, in Update() method, a module may obtain data
+    from other modules by calling upon corresponding methods,
+    but it may not set any data for other modules.
+    This rule will lead to a result that a module only connects
+    to the modules it rely upon, but not to those rely on it.
+    In this way, the relationship of modules are made clear,
+    and the order of update can be decided.
 */
 
 #ifndef DOG_CONTROL_UTILS_CLASSDECLARE_H
